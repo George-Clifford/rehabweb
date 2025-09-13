364c555c3665526aa6ea8a372318eb0e182095ca
George Clifford <georgecliffordodiwuor@gmail.com>
2025-09-13 23:28:44 +0300

    remodified contact page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Header from './components/Header'
 import About from './components/About'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+ import { ToastContainer } from 'react-toastify';
+ //import' react-toastify/dist/ReacttToastify.css'
 
 const App = () => {
   return (
     <div className=''>
+      <ToastContainer/>
       <Navbar/> 
       <Header/>
       <About/>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,55 +1,78 @@
-// import React from 'react'
-
-// const Contact = () => {
-//   return (
-//     <div className='container mx-auto py-4 pt-20 px-6 md:px-20
-//     lg:px-32 my-20 w-full overflow-hidden' id='Contact'>
-//         <h1 className='text-3xl font-bold text-green-600 sm:text-4xl mb-8 text-center '>Contact   <span className='text-purple-400 underline underline-offset-4 '>Us</span></h1>
-//         <p className='text-center mb-8 max-w-80 mx-auto text-2xl'>We are located In <span className='font-bold text-green-600'>Migori Town</span> along <span className='font-bold text-purple-500'>Ombo Junction</span>.</p>
-//         <p className='text-center mb-8 max-w-80 mx-auto text-2xl'>You can also reach out to us via:</p>
-//         <p className='text-center mb-8 max-w-80 mx-auto text-2xl'>Phone: <span className='text-green-600 font-bold'>+254708354496</span></p>
-//         <p className='text-center mb-8 max-w-80 mx-auto text-2xl'>Email: <span className='text-green-600 font-bold'>recoveryatshrc@gmail.com</span></p>
-//     </div>
-
-//   )
-// }
-
-// export default Contact
-
 import React from 'react'
 import { FaPhoneAlt } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
+import { toast } from 'react-toastify';
 
 const Contact = () => {
+ const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "d708a655-96c6-4870-85db-4c12cc2da0ad");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success("Form Submitted Succesfully")
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message)
+      setResult();
+    }
+  };
+
+
   return (
-    <div 
-      className='container mx-auto py-4 pt-20 px-6 md:px-20
-      lg:px-32 my-20 w-full overflow-hidden' 
-      id='Contact'
-    >
+   
+    <div className='text-center p-6 py-20 lg;px-32 w-full overflow-hidden ' id='Contact'>
       <h1 className='text-3xl font-bold text-green-600 sm:text-4xl mb-8 text-center'>
-        Contact <span className='text-purple-400 underline underline-offset-4'>Us</span>
-      </h1>
+          Contact <span className='text-purple-400 underline underline-offset-4'>Us</span>
+     </h1>
+     <p className='text-center mb-12  max-w-80 mx-auto'>We are located in <span className='text-green-600 font-bold'>Migori Town</span> next to <span className='text-purple-500 font-bold'>Oruba Primary</span>.</p>
+     <p className='mb-2'>You can also contact us via:</p>
 
-      <p className='text-center mb-8 max-w-80 mx-auto text-2xl'>
-        We are located In <span className='font-bold text-green-600'>Migori Town</span>  along <span className='font-bold text-purple-500'> Ombo Junction</span>.
-      </p>
+     <p className='flex items-center justify-center gap-3'>
+         <FaPhoneAlt className='text-green-600' />
+         <span className='text-green-600 font-bold'>+254708354496</span>
+    </p>
 
-      <p className='text-center mb-8 max-w-80 mx-auto text-2xl'>
-        You can also reach out to us via:
-      </p>
+     <p className='flex items-center justify-center gap-3 '>
+         <MdEmail className='text-green-600 text-3xl' />
+         <span className='text-green-600 font-bold'>recoveryatshrc@gmail.com</span>
+       </p>
 
-      {/* Phone */}
-      <p className='flex items-center justify-center gap-3 text-2xl mb-6'>
-        <FaPhoneAlt className='text-green-600' />
-        <span className='text-green-600 font-bold'>+254708354496</span>
-      </p>
+     <form onSubmit={onSubmit} className='max-w-2xl mx-auto pt-8'>
+      <div className='flex flex-wrap'>
+      <div className='w-full md:w-1/2 text-left'>
+        Name
+        <input className='w-full border border-gray-300 rounded py-3 
+        px-4 mt-2 ' type="text" name='Name' placeholder='Your Name' required/>
+      </div>
 
-      {/* Email */}
-      <p className='flex items-center justify-center gap-3 text-2xl'>
-        <MdEmail className='text-green-600 text-3xl' />
-        <span className='text-green-600 font-bold'>recoveryatshrc@gmail.com</span>
-      </p>
+      <div className='w-full md:w-1/2 text-left'>
+        Email
+        <input className='w-full border border-gray-300 rounded py-3 
+        px-4 mt-2 ' type="email" name='Email' placeholder='Your Email' required/>
+      </div>
+      </div>
+      <div className='my-6 text-left '>
+        Message
+        <textarea className='w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none' name="Message" placeholder='Message' required></textarea>
+      </div>
+
+      <button className='bg-purple-500 text-white py-2 px-12 mb-10 rounded '> {result ? result: "Send Message"}</button>
+     </form>
+     
     </div>
   )
 }
